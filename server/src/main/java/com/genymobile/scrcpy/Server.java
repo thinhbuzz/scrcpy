1e788b3da1b3bf185644008070935f7694201d8e
Thinh Buzz <mr.thinhbuzz@gmail.com>
2025-06-21 14:58:39 +0700

    fix `Segmentation fault - 139` on android 15

--- server/src/main/java/com/genymobile/scrcpy/Server.java
+++ server/src/main/java/com/genymobile/scrcpy/Server.java
@@ -9,6 +9,8 @@ public final class Server {
         // not instantiable
     }
 
+    // adb push "C:\Users\thinhbuzz\Downloads\scrcpy-server-v3.3.1" /data/local/tmp/off-screen-3.3.1
+    // adb push /release/off-screen-3.3.1" /data/local/tmp/off-screen-3.3.1
     // export offScreenPath=/data/local/tmp/off-screen-3.3.1
     // [[ -f $offScreenPath ]] && CLASSPATH=$offScreenPath app_process / com.genymobile.scrcpy.Server on
 
@@ -22,5 +24,6 @@ public final class Server {
             boolean setPowerModeOk = Device.setDisplayPower(true);
             Ln.i("Device screen turned on " + (setPowerModeOk ? "true" : "false"));
         }
+        System.exit(0);
     }
 }
